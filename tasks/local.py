c57084c0bec75a846146878031c604d2a71fefb6
Alejandro Mc Ewen <alejandro.mcewen@macheight.com>
2023-09-03 09:52:38 -0500

    Making testing easier

--- tasks/local.py
+++ tasks/local.py
@@ -59,7 +59,7 @@ def test_webapp(c):
 
 def build_and_start(c):
     c.run("docker-compose build")
-    c.run("docker-compose up -d")
+    c.run("docker-compose up")
     # Make sure yarn install has been completed and postgres DB created
     print("[INFO] Waiting for Front-end app to respond")
     while True:
