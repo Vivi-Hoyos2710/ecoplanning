ea9f2dd49c959df5f5aa17d6f99046ae4b80b646
csofia1408 <csrendonb@eafit.edu.co>
2023-09-08 15:19:15 -0500

    login Start Viviana Carla

---
+++ frontend/src/components/home/LoginFondo.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+const LoginFondo = () => {
+    return(<div className="bg-[url(../img/fondoLogin.svg')]"></div>);
+};
+export default LoginFondo;
