1c47f7efad958a19c22e5b570a8c9c56a9de62c3
Alejandro Mc Ewen <alejandro.mcewen@macheight.com>
2023-09-14 11:24:16 -0500

    Adding service

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -17,3 +17,11 @@ export async function createUser(user: UserInfo): Promise<UserInfo>  {
     );
     return response.data
 }
+
+export async function validateUser(user: UserInfo): Promise<UserInfo>  {
+    const response = await axios.post<UserInfo>(
+        'http://127.0.0.1:8000/user/validation/',
+        user
+    );
+    return response.data
+}
